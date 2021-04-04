d279028d8196e1c44a8e30978f78e69856898e1c
Facundot96 <facundotapia.96@gmail.com>
2021-04-04 19:02:35 -0300

    se sube el archivo finalizado

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
-int main(){
+int main() {
 
-    return 0;
+  int i = 10;
+  int *punteroI;
+
+  punteroI = &i;
+
+  printf("\nContenido de la variable i: %d", i);
+  printf("\nContenido del puntero: %d", *punteroI);
+  printf("\nDirección de memoria almacenada en el puntero: %p", punteroI);
+  printf("\nDirección de memoria de la variable i: %p", &i);
+  printf("\nDirección de memoria del puntero: %p", &punteroI);
+  printf("\n\nTamaño de memoria utilizado por i: %lu", sizeof(i));
+
+  return 0;
 }
